ce8b49aa348343d21a038f627b105f2c15b304e2
Vishwaka99 <166687279+Vishwaka99@users.noreply.github.com>
2025-05-02 15:39:05 +0530

    Completed Course Registration System

---
+++ Course.java
@@ -0,0 +1,23 @@
+public class Course {
+    private String id;
+    private String name;
+    private String instructor;
+
+    public Course(String id, String name, String instructor) {
+        this.id = id;
+        this.name = name;
+        this.instructor = instructor;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+}
